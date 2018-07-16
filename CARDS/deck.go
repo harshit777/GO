e79c709bc8b6d27fd36c6b5fcf1dea03ce8532dc
harshit777 <harshit.g.0702@gmail.com>
2018-07-16 17:58:15 +0530

    shuffling function with different seed value

--- CARDS/deck.go
+++ CARDS/deck.go
@@ -5,8 +5,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of deck
@@ -78,4 +80,17 @@ func newDeckFromFile(filename string) deck {
 	s := strings.Split(string(bs), ",")
 	return deck(s)
 
+}
+
+//shuffling of the cards
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- CARDS/main.go
+++ CARDS/main.go
@@ -3,8 +3,12 @@ package main
 func main() {
 
 	/*	cards := newDeck()
-		cards.saveToFile("my_cards")*/
+			cards.saveToFile("my_cards")
 
-	cards := newDeckFromFile("my_cards")
+		cards := newDeckFromFile("my_cards")
+		cards.print()*/
+
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
